457052f157da10f63b860a817f0873f5427fcfd5
Spencer Smith <ssmith@sofi.org>
2019-04-29 14:57:56 -0600

    Explore graph and create spanning tree

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const DIMENTION = 10;
 
 const start = 0;
 const graph = [];
+const tree = [];
 const visited = {};
 
 // fill graph
@@ -15,15 +16,36 @@ for (let i = 0; i < DIMENTION * DIMENTION; i++) {
 // add connections
 for (let i = 0; i < DIMENTION * DIMENTION; i++) {
   const node = graph[i];
-  const { row, column } = toRowColumn(i);
+  const { up, down, left, right } = getNeighbors(i);
+  const edges = [up, down, right, left].filter(e => e);
+  node.edges = edges;
+}
 
+explore(0);
+console.log('tree:', tree);
+
+function explore(index) {
+  visited[index] = true;
+
+  const { up, down, left, right } = getNeighbors(index);
+  let unvisited = [up, down, right, left].filter(n => n && !visited[n]);
+
+  while(unvisited.length > 0) {
+    const randIndex = Math.floor(Math.random() * unvisited.length);
+    const next = unvisited[randIndex];
+    tree.push([index, next]); // add pair to tree
+    explore(next);
+    unvisited.splice(randIndex, 1); // remove from unvisited
+  }
+}
+
+function getNeighbors(index) {
+  const { row, column } = toRowColumn(index);
   const up = toIndex(row - 1, column);
   const down = toIndex(row + 1, column);
   const left = toIndex(row, column - 1);
   const right = toIndex(row, column + 1);
-
-  const edges = [up, down, right, left].filter(e => e);
-  node.edges = edges;
+  return { up, down, left, right };
 }
 
 function toRowColumn(index) {
